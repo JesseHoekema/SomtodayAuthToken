b0e734d1038774e525924859e30fbdd0b358c5dd
Jesse Hoekema <109305882+JesseHoekema@users.noreply.github.com>
2025-10-06 13:54:13 +0200

    Create accessandrefreshtoken.js

---
+++ extras/accessandrefreshtoken.js
@@ -0,0 +1,26 @@
+// this is bookmarklet to only get access token ! refresh token
+javascript:(function(){
+    try {
+        var tokenInfo = '';
+        for(var i=0;i<localStorage.length;i++){
+            var key = localStorage.key(i);
+            if(key && key.indexOf('CapacitorStorage.') === 0){
+                try{
+                    var parsed = JSON.parse(localStorage.getItem(key));
+                    if(parsed && (parsed.access_token || parsed.expires_at || parsed.refresh_token)){
+                        tokenInfo = (parsed.access_token||'') + '!' + (parsed.refresh_token||'');
+                        break;
+                    }
+                }catch(e){}
+            }
+        }
+        if(!tokenInfo){
+            alert('No token info found!');
+            return;
+        }
+        var targetURL = 'https://projects.jessehoekema.com/SomtodayAuthToken?tokenInfo=' + encodeURIComponent(tokenInfo);
+        window.location.href = targetURL;
+    } catch(e){
+        alert('Error: ' + e);
+    }
+})();
