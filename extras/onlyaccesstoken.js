339027d298113a6477a2afc31d3e52f65b253e08
Jesse Hoekema <109305882+JesseHoekema@users.noreply.github.com>
2025-10-06 13:52:46 +0200

    Create onlyaccesstoken.js

---
+++ extras/onlyaccesstoken.js
@@ -0,0 +1,26 @@
+// this is the bookmarklet script to only get the access token from somtoday
+javascript:(function(){
+    try {
+        var tokenInfo = '';
+        for(var i=0;i<localStorage.length;i++){
+            var key = localStorage.key(i);
+            if(key && key.indexOf('CapacitorStorage.') === 0){
+                try{
+                    var parsed = JSON.parse(localStorage.getItem(key));
+                    if(parsed && (parsed.access_token || parsed.expires_at || parsed.refresh_token)){
+                        tokenInfo = (parsed.access_token||'')
+                        break;
+                    }
+                }catch(e){}
+            }
+        }
+        if(!tokenInfo){
+            alert('No token info found!');
+            return;
+        }
+        var targetURL = 'https://projects.jessehoekema.com/SomtodayAuthToken?tokenInfo=' + encodeURIComponent(tokenInfo);
+        window.location.href = targetURL;
+    } catch(e){
+        alert('Error: ' + e);
+    }
+})();
